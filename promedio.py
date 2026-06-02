0923bac40859c5e5eb6183b35e375959f326fa43
JOSUE ULCUANGO <josue.ulcuango@unach.edu.ec>
2026-06-01 19:44:29 -0500

    Implement average calculation function
    Added a function to calculate the average of a list of numbers and included example usage.

--- promedio.py
+++ promedio.py
@@ -1,1 +1,20 @@
+def calcular_promedio(lista_numeros):
+    """
+    Calcula el promedio de una lista de números.
 
+    Parámetros:
+    lista_numeros (list): Lista que contiene valores numéricos.
+
+    Retorna:
+    float: El promedio de los números de la lista.
+    """
+
+    promedio = sum(lista_numeros) / len(lista_numeros)
+    return promedio
+
+
+notas = [8, 9, 10, 7, 6]
+resultado = calcular_promedio(notas)
+
+print("Las notas son:", notas)
+print("El promedio es:", resultado)
